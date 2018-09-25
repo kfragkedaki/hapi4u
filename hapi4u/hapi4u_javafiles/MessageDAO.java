ff6083d9de1770d1784dd813744c9810b265bd61
Theonimfi <theon.anog@gmail.com>
2018-09-25 22:55:08 +0300

    Add messages and messagesDAO class for messages

---
+++ hapi4u/hapi4u_javafiles/MessageDAO.java
@@ -0,0 +1,68 @@
+package hapi4u_javafiles;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * messageDAO provides all the necessary methods for "handling" message's data.
+ */
+public class MessageDAO {
+
+	/**
+	 * This method returns a List with all Messages
+	 *
+	 * @return List<User>
+	 */
+	public List<Message> getMessagesByPharmacyId(int pharmacy_id) throws Exception {
+
+		Connection con = null;
+
+		String sqlquery= "SELECT * FROM messages WHERE pharmacy_id=?;";
+
+		DB db = new DB();
+		List<Message> messages = new ArrayList<Message>();
+
+		try {
+
+			db.open(); //open connection
+
+			con = db.getConnection(); //get Connection object
+
+			PreparedStatement stmt = con.prepareStatement(sqlquery);
+			stmt.setInt( 1, pharmacy_id );
+
+			ResultSet rs = stmt.executeQuery();
+
+			while ( rs.next() ) {
+				messages.add( new Message( rs.getInt("message_id"), rs.getString("title"), rs.getString("name"), rs.getString("email"), rs.getString("message"), rs.getInt("pharmacy_id"), rs.getInt("admin_id"), rs.getBoolean("checked") ) );
+			}
+
+			rs.close(); //closing ResultSet
+			stmt.close(); // closing PreparedStatement
+			db.close(); // closing connection
+
+			return messages;
+
+		} catch (Exception e) {
+
+			throw new Exception( e.getMessage() );
+
+		} finally {
+
+			try {
+				db.close();
+			} catch (Exception e) {
+				//no need to do anything...
+			}
+
+		}
+
+	}
+
+
+	public MessageDAO() {
+
+	}
+
+}
