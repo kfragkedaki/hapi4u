e1c3391200980d1a85328926cb52eb3ac8014a0c
Theonimfi <theon.anog@gmail.com>
2018-09-28 02:10:07 +0300

    Add delete and find message by admin id functions in MessageDAO

--- hapi4u/hapi4u_javafiles/MessageDAO.java
+++ hapi4u/hapi4u_javafiles/MessageDAO.java
@@ -60,6 +60,52 @@ public class MessageDAO {
 
 	}
 
+	public List<Message> getMessagesByAdminId(int admin_id) throws Exception {
+
+		Connection con = null;
+
+		String sqlquery= "SELECT * FROM messages WHERE admin_id=?;";
+
+		DB db = new DB();
+		List<Message> messages = new ArrayList<Message>();
+
+		try {
+
+			db.open(); //open connection
+
+			con = db.getConnection(); //get Connection object
+
+			PreparedStatement stmt = con.prepareStatement(sqlquery);
+			stmt.setInt( 1, admin_id );
+
+			ResultSet rs = stmt.executeQuery();
+
+			while ( rs.next() ) {
+				messages.add( new Message( rs.getString("title"), rs.getString("name"), rs.getString("email"), rs.getString("message"), rs.getInt("pharmacy_id"), rs.getInt("admin_id"), rs.getBoolean("checked") ) );
+			}
+
+			rs.close(); //closing ResultSet
+			stmt.close(); // closing PreparedStatement
+			db.close(); // closing connection
+
+			return messages;
+
+		} catch (Exception e) {
+
+			throw new Exception( e.getMessage() );
+
+		} finally {
+
+			try {
+				db.close();
+			} catch (Exception e) {
+				//no need to do anything...
+			}
+
+		}
+
+	}
+
 	public void saveMessage(Message message) {
 
 		Connection con = null;
@@ -112,6 +158,58 @@ public class MessageDAO {
 		}
 
 	} //End of saveMessage
+
+	public void deleteMessage(Message message) throws Exception {
+
+		Connection con = null;
+
+		String sqlquery= "DELETE FROM Table WHERE title=? , name=?, email=?, message=?, pharmacy_id=?, edmin_id=?, checked=?";
+
+		DB db = new DB();
+
+		try {
+
+			db.open(); //open connection
+
+			con = db.getConnection(); //get Connection object
+
+			PreparedStatement stmt = con.prepareStatement(sqlquery);
+			stmt.setString( 1, message.getTitle() );
+			stmt.setString( 2, message.getName() );
+			stmt.setString( 3, message.getEmail() );
+			stmt.setString( 4, message.getMessage() );
+			if (message.getPharmacyId() == 0){
+				stmt.setNull(5,java.sql.Types.INTEGER);
+			}else{
+				stmt.setInt( 5, message.getPharmacyId() );
+			}
+			if (message.getAdminId() == 0){
+				stmt.setNull(6,java.sql.Types.INTEGER);
+			}else{
+				stmt.setInt( 6, message.getAdminId() );
+			}
+			stmt.setBoolean( 7, message.getIfChecked() );
+
+			stmt.executeUpdate();
+
+			stmt.close(); // closing PreparedStatement
+			db.close(); // closing connection
+
+		} catch (Exception e) {
+
+			throw new Exception( e.getMessage() );
+
+		} finally {
+
+			try {
+				db.close();
+			} catch (Exception e) {
+				//no need to do anything...
+			}
+
+		}
+
+	}
 
 
 	public MessageDAO() {
