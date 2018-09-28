293cbbe46bc949112d3377f288874166f2a3b767
Theonimfi <theon.anog@gmail.com>
2018-09-28 14:33:39 +0300

    Add a messageId in Message class

--- hapi4u/hapi4u_javafiles/Message.java
+++ hapi4u/hapi4u_javafiles/Message.java
@@ -2,6 +2,7 @@ package hapi4u_javafiles;
 
 public class Message {
 
+	private int message_id;
 	private String title;
 	private String name;
 	private String email;
@@ -18,8 +19,9 @@ public class Message {
 	 * @param password, String
 	 * @param email, String
 	 */
-	public Message( String title, String name, String email, String message, int pharmacy_id, int admin_id, boolean checked) {
+	public Message( int message_id,String title, String name, String email, String message, int pharmacy_id, int admin_id, boolean checked) {
 
+		this.message_id = message_id;
 		this.title = title;
 		this.name = name;
 	    this.email = email;
@@ -30,6 +32,15 @@ public class Message {
 	}
 
 	/*Getters and Setters*/
+
+
+	public int getMessageId() {
+		return message_id;
+	}
+
+	public void setMessageId(int message_id) {
+		this.message_id = message_id;
+	}
 
 
 	public String getTitle() {
